67fbc62ec2fb792cb5d53b4df8d74fca814e8503
Julio César Gama <juliocesargama@protonmail.com>
2021-10-27 23:31:34 -0300

    Inclusão de Proxy CORS (Vercel)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API: 'https://jsoneditoronline.org/#left=cloud.c9efb4554bb74c3590d25c96ad7f0132'
+  API: 'https://cors-anywhere.herokuapp.com/https://jsoneditoronline.org/#left=cloud.c9efb4554bb74c3590d25c96ad7f0132'
 };
